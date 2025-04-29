054e19ae650194f47b142f45c7c9b1a6c8b5b4af
Robin Feuz <rfeuz@ethz.ch>
2025-04-29 16:20:44 +0200

    Initial Python Script

---
+++ run_optiver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import sys
+import numpy as np
+import torch
+
+np.finfo(np.dtype("float32"))
+np.finfo(np.dtype("float64"))
+
+
+
+
+
+def main():
+    # Main Code
+    print("Start Optiver...")
+
+
+if __name__ == "__main__":
+    main()
